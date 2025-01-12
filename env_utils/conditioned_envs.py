6fee555c47e160ed864d29c5d0b8b36a21a9fddc
Hosein <hosein.hasani@sharif.edu>
2025-01-12 23:41:38 +0330

    Add a buffer of previous samples to wrapper

--- env_utils/conditioned_envs.py
+++ env_utils/conditioned_envs.py
@@ -2,12 +2,15 @@ import gymnasium as gym
 import numpy as np
 
 class ConditionalStateWrapper(gym.ObservationWrapper):
-    def __init__(self, env, context_size=7):
+    def __init__(self, env, context_size=7, trajectory_limit=100):
         super().__init__(env)
-        
+
         self.context_size = context_size
         self.context = np.zeros(context_size, dtype=np.float32)
         
+        self.trajectory_limit = trajectory_limit
+        self.trajectory_buffer = []  
+
         obs_space = self.env.observation_space
         self.observation_space = gym.spaces.Box(
             low=np.concatenate([obs_space.low, -np.inf * np.ones(context_size)]),
@@ -22,3 +25,22 @@ class ConditionalStateWrapper(gym.ObservationWrapper):
         assert len(context) == self.context_size, \
             f"Context must have size {self.context_size}, but got size {len(context)}."
         self.context = np.array(context, dtype=np.float32)
+
+    def step(self, action):
+        next_obs, reward, done, info = self.env.step(action)
+
+        state = self.env.state
+        self._add_to_buffer((state, action, reward))
+
+        return self.observation(next_obs), reward, done, info
+
+    def reset(self, **kwargs):
+        self.trajectory_buffer = []
+        initial_obs = self.env.reset(**kwargs)
+        return self.observation(initial_obs)
+
+    def _add_to_buffer(self, entry):
+        self.trajectory_buffer.append(entry)
+        if len(self.trajectory_buffer) > self.trajectory_limit:
+            self.trajectory_buffer.pop(0)  
+            
