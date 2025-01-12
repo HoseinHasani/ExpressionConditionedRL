079717998916fc589561f4429139c3bf33a0e008
Hosein <hosein.hasani@sharif.edu>
2025-01-12 23:58:51 +0330

    Run stable_baseline on the manual environment

--- stb_sac_halfcheetah.py
+++ stb_sac_halfcheetah.py
@@ -4,21 +4,28 @@ import matplotlib.pyplot as plt
 from stable_baselines3 import SAC
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.monitor import Monitor
-import os
+from env_utils.conditioned_envs import ConditionalStateWrapper
 from stable_baselines3.common.callbacks import EvalCallback
 import gymnasium
+import os
+from envs.reacher import GoalReacherEnv
 
 sac_log_dir = './sac_logs/'
 os.makedirs(sac_log_dir, exist_ok=True)
 
 
-env = make_vec_env('HalfCheetah-v4', n_envs=4, wrapper_class=lambda e: Monitor(e, sac_log_dir))
+#env = gymnasium.make("HalfCheetah-v4") #make_vec_env('HalfCheetah-v4', n_envs=4, wrapper_class=lambda e: Monitor(e, sac_log_dir))
+env = GoalReacherEnv()
+env = ConditionalStateWrapper(env)
 
 sac_model = SAC('MlpPolicy', env, verbose=1, learning_rate=0.001)
-eval_env = gymnasium.make("HalfCheetah-v4")
+# eval_env = gymnasium.make("HalfCheetah-v4")
+eval_env = GoalReacherEnv()
+eval_env = ConditionalStateWrapper(eval_env)
+eval_env = Monitor(eval_env)
 
-eval_callback_sac = EvalCallback(eval_env, best_model_save_path='./sac_logs/',
-                             log_path='./sac_logs/', eval_freq=100,
+eval_callback_sac = EvalCallback(eval_env, best_model_save_path=sac_log_dir,
+                             log_path=sac_log_dir, eval_freq=100,
                              deterministic=True, render=False)
 sac_model.learn(total_timesteps=10000, callback=eval_callback_sac)
 
