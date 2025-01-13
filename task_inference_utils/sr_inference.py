3641d930fc82236bf7ef61add3fcfaf0d094c1c6
Hosein <hosein.hasani@sharif.edu>
2025-01-13 12:09:36 +0330

    Symbolic regression based task inference module

---
+++ task_inference_utils/sr_inference.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pysindy as ps
+from task_inference_utils.base_inference import BaseTaskInference
+
+class SymbolicRegressionInference(BaseTaskInference):
+    def __init__(self, context_size, feature_lib=None, optimizer=None):
+
+        super().__init__(context_size)
+        self.feature_lib = feature_lib or ps.PolynomialLibrary(degree=2)
+        self.optimizer = optimizer or ps.STLSQ(threshold=0.1)
+        self.state_data = []
+        self.action_data = []
+
+    def infer_task(self, trajectory_buffer):
+
+        if len(trajectory_buffer) < 5:
+            return np.zeros(self.context_size, dtype=np.float32)
+
+        states, actions, rewards = zip(*trajectory_buffer)
+        states = np.array(states)
+        actions = np.array(actions)
+
+        x_train = states[:-1]
+        u_train = actions[:-1]
+        y_train = states[1:]
+        
+        model = ps.SINDy(discrete_time=True, 
+                         feature_library=self.feature_lib, 
+                         optimizer=self.optimizer)
+        model.fit(x_train, u=u_train, x_dot=y_train, 
+                  multiple_trajectories=False, quiet=True)
+
+        coeffs = model.coefficients()
+        flat_coeffs = np.concatenate(coeffs, axis=None).astype(np.float32)
+
+        return flat_coeffs
